bb3f44a68b32d1bd4aef1dbad39e0efb7327ad51
smolvik1 <smolvik@hotmail.com>
2025-01-02 14:57:23 +0100

    rename

--- model/well.py
+++ model/well.py
@@ -1,25 +1,38 @@
 from pydantic import BaseModel, Field, ValidationError, field_validator
-from typing import Dict, Optional
+from typing import Dict, Optional, Literal
 import yaml
 import os
 
 VALID_YAML_PATH = os.path.join("attributes", "well_attributes.yaml")
+
+af_categories = Literal[
+    "Calculated",
+    "Control and Monitoring",
+    "ESP",
+    "Flow Meter",
+    "Gas Lift",
+    "ICV",
+    "Info",
+    "Sand",
+    "Valves",
+]
+
 
 class Attribute(BaseModel):
     alias: str = Field(..., min_length=1)
     color: Optional[str] = None
     description: str = Field(..., min_length=1)
     identifier: str = Field(..., min_length=1)
-    af_category: Optional[str] = None
-    af_category_2: Optional[str] = None
+    af_category: af_categories
+    af_category_2: Optional[af_categories] = None
 
     @field_validator("identifier", "alias", "description", mode="before")
     @classmethod
     def check_not_empty(cls, value, field):
         if not value:
-            raise ValueError(f'Error: {field} must not be empty')
+            raise ValueError(f"Error: {field} must not be empty")
         return value
-    
+
 
 class WellAttributes(BaseModel):
     attribute: Dict[str, Attribute]
@@ -31,18 +44,17 @@ class WellAttributes(BaseModel):
         seen_alias = set()
 
         for key, attr in value.items():
-            identifier = attr['identifier']
-            alias = attr['alias']
+            identifier = attr["identifier"]
+            alias = attr["alias"]
 
             if identifier in seen_identifiers:
                 raise ValueError(f"Duplicate identifier found: {identifier}")
             if alias in seen_alias:
                 raise ValueError(f"Duplicate alias found: {alias}")
-            
+
             seen_identifiers.add(identifier)
             seen_alias.add(alias)
         return value
-
 
 
 # Load YAML file and validate
